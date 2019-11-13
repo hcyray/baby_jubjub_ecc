95ffdd07ae09dac0a0333f9c6ee6d588ba7940df
hcyray <chuangak@connect.ust.hk>
2019-11-13 23:39:36 +0800

    update api

---
+++ work.sh
@@ -0,0 +1,12 @@
+cp -avr baby_jubjub_ecc/ ~/hkust/go_project/src/github.com/uchihatmtkinu/PriRC/snark/
+cd cmake-build-default/baby_jubjub_ecc 
+cp libbaby_jubjub_ecc.a ~/hkust/go_project/src/github.com/uchihatmtkinu/PriRC/snark	
+cd ..
+cd depends/libsnark/libsnark
+cp libsnark.a ~/hkust/go_project/src/github.com/uchihatmtkinu/PriRC/snark
+cd ..
+cd depends/libff/libff 
+cp libff.a ~/hkust/go_project/src/github.com/uchihatmtkinu/PriRC/snark
+
+
+
